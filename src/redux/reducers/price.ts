dd900c3bd23c166d77155b4fe8c8732f80ac2c1e
Egill Hreinson <egillh210@gmail.com>
2019-11-07 16:33:23 +0100

    linting graph

--- src/redux/reducers/price.ts
+++ src/redux/reducers/price.ts
@@ -3,6 +3,8 @@ import { Prices } from '../../models/prices'
 
 const pricesInitialState: Prices = [{ ticker: 'aapl', change: 0, changePercent: 0, latestPrice: 0, error: false }]
 
+// const pricesInitialState: Prices | [] = [];
+
 export const prices = (state = pricesInitialState, action: UpdatePricesDataAction) => {
     const { type, payload } = action;
     switch (type) {
